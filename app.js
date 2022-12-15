b03d8cc4fd321540384ec63bc9303e8833252da3
chriskimty <christaeyoung.kim@gmail.com>
2022-12-15 15:18:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// Your users should be able to:
+    // Select and submit a number rating
+    // See the "Thank you" card state after submitting a rating
+    // See hover and focus states for all interactive elements on the page
